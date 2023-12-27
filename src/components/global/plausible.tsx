41856ab20a5c189daf6b734c2750ba0ff3862883
Oeponn <tigershi0110@gmail.com>
2023-12-27 17:23:21 -0500

    tracker apiHost

--- src/components/global/plausible.tsx
+++ src/components/global/plausible.tsx
@@ -2,6 +2,7 @@ import plausibleTracker from 'plausible-tracker';
 
 const plausible = plausibleTracker({
   domain: 'undrstand.me', // Replace with your domain
+  apiHost: 'https://plausible.undrstand.me',
   // You can add additional options here if needed
 });
 
